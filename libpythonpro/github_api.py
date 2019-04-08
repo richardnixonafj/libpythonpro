f3cb82b241d08a167d3842e0a948720c36585e08
Richard Nixon <richardnixon@posteo.org>
2019-04-07 21:29:01 -0300

    Criacao de algoritmo que recupera foto de perfil da api do github

---
+++ libpythonpro/github_api.py
@@ -0,0 +1,16 @@
+import requests
+
+def busca_avatar(usuario):
+    """
+    Buscar avatar de usuario no github
+
+    :param usuario: str com nome do usuario no github
+    :return: str com o link do avatar
+    """
+    url = f'https://api.github.com/users/{usuario}'
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
+
+if __name__ == '__main__':
+    print(busca_avatar('sirpresident'))
+
